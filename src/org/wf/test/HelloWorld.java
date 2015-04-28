aa62e784db1913ca6ec4f9db2df332557738682d
wenfeng1989 <1106480823@qq.com>
2015-04-28 21:33:36 +0800

    建立分支

--- src/org/wf/test/HelloWorld.java
+++ src/org/wf/test/HelloWorld.java
@@ -1,10 +1,14 @@
 package org.wf.test;
-
+/**
+ * 
+ * @author wf
+ *
+ */
 public class HelloWorld {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
 	}
 
 }
